af11de7420ac2a4aedccd6dcb6f7b6cfb926907f
raf-wtag <fahim.rahman@welldev.io>
2021-06-23 17:25:15 +0600

    Project Initialized

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Fahim Rahman on 23/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
